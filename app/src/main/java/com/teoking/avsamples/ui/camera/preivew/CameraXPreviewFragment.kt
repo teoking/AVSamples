54049809f2f68b159bba5bd63929a9ea515d698e
teoking <yagatong@gmail.com>
2020-05-15 15:22:40 +0800

    Add CameraX sample

---
+++ app/src/main/java/com/teoking/avsamples/ui/camera/preivew/CameraXPreviewFragment.kt
@@ -0,0 +1,62 @@
+package com.teoking.avsamples.ui.camera.preivew
+
+import android.Manifest
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+
+import com.teoking.avsamples.R
+import com.teoking.common.PermissionFragment
+import kotlinx.android.synthetic.main.fragment_camera_x_preview.*
+
+class CameraXPreviewFragment : PermissionFragment(
+    perms = arrayOf(
+        Manifest.permission.CAMERA
+    )
+) {
+
+    companion object {
+        fun newInstance() = CameraXPreviewFragment()
+    }
+
+    private lateinit var viewModel: CameraXPreviewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera_x_preview, container, false)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        // TODO To be supported.
+        // A good start to refer for this task:
+        // https://github.com/android/camera-samples/blob/master/CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/CameraFragment.kt
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CameraXPreviewViewModel::class.java)
+        descTextView.text = viewModel.descText
+
+        previewSelectGroup.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId) {
+                R.id.surfaceViewButton -> {
+                    viewModel.requestWithSurfaceView(this, preview_view)
+                }
+                R.id.textureViewButton -> {
+                    viewModel.requestWithTextureView(this, preview_view)
+                }
+                else -> {
+                    Toast.makeText(context, "Not implemented!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/teoking/avsamples/ui/camera/preivew/CameraXPreviewViewModel.kt
@@ -0,0 +1,112 @@
+package com.teoking.avsamples.ui.camera.preivew
+
+import android.app.Application
+import android.util.Log
+import android.util.Size
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.viewModelScope
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+class CameraXPreviewViewModel(application: Application) : AndroidViewModel(application) {
+
+    val descText = "This sample shows how to using CameraX API to implement Camera Preview/Capture"
+
+    private lateinit var _cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var _cameraProvider: ProcessCameraProvider
+
+    /** Blocking camera operations are performed using this executor */
+    private var cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+
+    /** Request previewing with a SurfaceView */
+    fun requestWithSurfaceView(lifecycleOwner: LifecycleOwner, previewView: PreviewView) {
+        viewModelScope.launch(Dispatchers.Main) {
+            val cameraProvider = fetchCameraProvider()
+            unbindUseCase(cameraProvider)
+            bindCameraUseCases(lifecycleOwner, previewView, PreviewView.ImplementationMode.SURFACE_VIEW, cameraProvider)
+        }
+    }
+
+    private fun bindCameraUseCases(
+        lifecycleOwner: LifecycleOwner,
+        previewView: PreviewView,
+        implementationMode: PreviewView.ImplementationMode,
+        cameraProvider: ProcessCameraProvider
+    ) {
+        previewView.preferredImplementationMode = implementationMode
+
+        val preview : Preview = Preview.Builder()
+            .setTargetResolution(RESOLUTION_SIZE)
+            .build()
+
+        // ImageAnalysis
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(RESOLUTION_SIZE)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                    // Values returned from our analyzer are passed to the attached listener
+                    // We log image analysis results here - you should do something useful
+                    // instead!
+                    Log.d(TAG, "Average luminosity: $luma")
+                })
+            }
+
+        val cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        val camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalysis)
+
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.cameraInfo))
+    }
+
+    private suspend fun fetchCameraProvider(): ProcessCameraProvider = suspendCoroutine { cont ->
+        _cameraProviderFuture = ProcessCameraProvider.getInstance(getApplication())
+
+        // Check for CameraProvider availability
+        _cameraProviderFuture.addListener(Runnable {
+            _cameraProvider = _cameraProviderFuture.get()
+            cont.resume(_cameraProvider)
+        }, ContextCompat.getMainExecutor(getApplication()))
+    }
+
+    private fun unbindUseCase(cameraProvider: ProcessCameraProvider) {
+        cameraProvider.unbindAll()
+    }
+
+    /** Request previewing with a TextureView */
+    fun requestWithTextureView(
+        lifecycleOwner: LifecycleOwner,
+        previewView: PreviewView
+    ) {
+        viewModelScope.launch(Dispatchers.Main) {
+            val cameraProvider = fetchCameraProvider()
+            unbindUseCase(cameraProvider)
+            bindCameraUseCases(lifecycleOwner, previewView, PreviewView.ImplementationMode.TEXTURE_VIEW, cameraProvider)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        // Shut down our background executor
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private val RESOLUTION_SIZE = Size(640, 480)
+        private val TAG = "CameraXPreviewViewModel"
+    }
+}
