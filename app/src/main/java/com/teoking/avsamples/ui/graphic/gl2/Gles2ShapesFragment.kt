9cdc13583dcffe6e1481c550df10e38d50adef38
teoking <yagatong@gmail.com>
2020-05-18 18:01:28 +0800

    Add GLES2.0 Sample

---
+++ app/src/main/java/com/teoking/avsamples/ui/graphic/gl2/Gles2ShapesFragment.kt
@@ -0,0 +1,36 @@
+package com.teoking.avsamples.ui.graphic.gl2
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+import com.teoking.avsamples.R
+import kotlinx.android.synthetic.main.fragment_gles2_shapes.*
+
+class Gles2ShapesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = Gles2ShapesFragment()
+    }
+
+    private lateinit var viewModel: Gles2ShapesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_gles2_shapes, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(Gles2ShapesViewModel::class.java)
+
+        descTextView.text = viewModel.descText
+    }
+
+}
---
+++ app/src/main/java/com/teoking/avsamples/ui/graphic/gl2/Gles2ShapesViewModel.kt
@@ -0,0 +1,8 @@
+package com.teoking.avsamples.ui.graphic.gl2
+
+import androidx.lifecycle.ViewModel
+
+class Gles2ShapesViewModel : ViewModel() {
+
+    val descText = "This sample shows how to draw shapes with OpenGL ES 2.0 Wrapper Api of Android"
+}
